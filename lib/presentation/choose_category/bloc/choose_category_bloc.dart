a5ea4a7db44f644b9f386f4d4d13b3b79aad942d
Djordje Nikolic <48301710+djordjenikolic960@users.noreply.github.com>
2024-04-01 12:00:04 +0200

    - choose category bloc added

---
+++ lib/presentation/choose_category/bloc/choose_category_bloc.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../domain/model/category.dart';
+import 'choose_category_event.dart';
+import 'choose_category_state.dart';
+
+class ChooseCategoryBloc
+    extends Bloc<ChooseCategoryEvent, ChooseCategoryState> {
+  final List<Category> _selectedCategories = [];
+
+  ChooseCategoryBloc() : super(CategoriesUpdated(const [])) {
+    on<CategorySelected>(_onCategorySelected);
+    on<CategoryDeselected>(_onCategoryDeselected);
+    on<SaveCategories>(_onSaveCategories);
+  }
+
+  FutureOr<void> _onCategorySelected(
+    CategorySelected event,
+    Emitter<ChooseCategoryState> emit,
+  ) {
+    _selectedCategories.add(event.category);
+    emit(CategoriesUpdated(List.from(_selectedCategories)));
+  }
+
+  FutureOr<void> _onCategoryDeselected(
+    CategoryDeselected event,
+    Emitter<ChooseCategoryState> emit,
+  ) {
+    _selectedCategories.removeWhere(
+        (category) => category.category.name == event.category.category.name);
+    emit(CategoriesUpdated(List.from(_selectedCategories)));
+  }
+
+  FutureOr<void> _onSaveCategories(
+    SaveCategories event,
+    Emitter<ChooseCategoryState> emit,
+  ) {
+    //todo emit use case to save this to pref of db
+    /*final SharedPreferences prefs = await SharedPreferences.getInstance();
+    // Assuming Category can be easily converted to/from JSON
+    List<String> jsonList = selectedCategories
+        .map((category) =>
+        jsonEncode({'id': category.id, 'name': category.name}))
+        .toList();
+    await prefs.setStringList('selectedCategories', jsonList);*/
+  }
+}
---
+++ lib/presentation/choose_category/bloc/choose_category_event.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../../../domain/model/category.dart';
+
+@immutable
+abstract interface class ChooseCategoryEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class CategorySelected extends ChooseCategoryEvent {
+  final Category category;
+
+  CategorySelected(this.category);
+
+  @override
+  List<Object?> get props => [category];
+}
+
+class CategoryDeselected extends ChooseCategoryEvent {
+  final Category category;
+
+  CategoryDeselected(this.category);
+
+  @override
+  List<Object?> get props => [category];
+}
+
+class SaveCategories extends ChooseCategoryEvent {}
